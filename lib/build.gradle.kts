4813e961ed12c3136f26fcda20e4d4037c260166
Paul Weber <work@paul-weber.de>
2022-09-19 11:32:55 +0200

    Change publication gradle scripts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -68,7 +68,7 @@ kapt {
 extra.apply{
     set("PUBLISH_GROUP_ID", "de.contentpass")
     set("PUBLISH_ARTIFACT_ID", "contentpass-android")
-    set("PUBLISH_VERSION", "2.0.0")
+    set("PUBLISH_VERSION", "2.1.0")
 }
 
 apply("${rootProject.projectDir}/scripts/publish-module.gradle")
